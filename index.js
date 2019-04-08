fa1c3942a764212511eb570f776215917866e512
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-08 13:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ User.prototype.veto = function(){
 // User.prototype.approve = function(){
 //   return 'You can do that!'
 // };
-// 
+//
 // User.prototype.doCharity = function(){
 //   return "I like to help people."
 // };
-// 
+//
 // User.prototype.releasePressStatement = function(){
 //   return "You will see great things from Scuber."
 // };
-// 
+//
 // User.prototype.sayHi = function(){
 //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 // };
