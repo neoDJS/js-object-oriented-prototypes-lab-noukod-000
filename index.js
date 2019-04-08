b115a376191426411af0db9a9754cbc6eca8ef57
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-08 13:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-User.prototype; 
+User.prototype;
 
 User.prototype.veto = function(){
   return 'No, I must disagree'
